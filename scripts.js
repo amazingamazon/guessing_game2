76f54471c90399dfc538ed802e3c788464bf577a
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-09 14:13:54 -0700

    Updated Game object to include guess var and play method. Minor HTML edits.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 var Game = function(name){
   this.name = name;
   this.answer = 7;
+
+  var guess = parseInt(prompt("Hi " + this.name + "! How many ingredients are in my french toast recipe? If you get it right, I'll give you the recipe."));
+
+  this.play = function play() {
+    if (guess === this.answer) {
+      alert("That's right! Let's have breakfast, " + this.name + ".");
+      message = "The ingredients are: 1 egg, 1 tsp vanilla, 1/2 tsp cinnamon, 1/4 cup milk, 4 slices of bread, butter, syrup. ";
+      message+= "Beat egg, vanilla and cinnamon in a bowl. Pour in milk. Dip bread in mixture and cook on medium heat until brown on both sides. ";
+      message+= "Top with butter and syrup, and enjoy, " + this.name + ".";
+      recipe = document.getElementById("recipe");
+      recipe.textContent = message;
+    } else if (guess > this.answer) {
+      alert("Too high! Guess a lower number, " + this.name + ".");
+    } else {
+      alert("Too low! Guess a higher number, " + this.name + ".");
+    }
+  }
 };
 
 var game = new Game("Jeff");
 
-var guess = prompt("Hi " + game.name + "! How many ingredients are in my french toast recipe? If you get it right, I'll give you the recipe.");
-
-function play() {
-  if (guess == game.answer) {
-    alert("That's right! Let's have breakfast, " + game.name + ".");
-    message = "The ingredients are: 1 egg, 1 tsp vanilla, 1/2 tsp cinnamon, 1/4 cup milk, 4 slices of bread, butter, syrup. ";
-    message+= "Beat egg, vanilla and cinnamon in a bowl. Pour in milk. Dip bread in mixture and cook on medium heat until brown on both sides. ";
-    message+= "Top with butter and syrup, and enjoy, " + game.name + ".";
-    recipe = document.getElementById("recipe");
-    recipe.textContent = message;
-  } else if (guess > game.answer) {
-    alert("Too high! Guess a lower number, " + game.name + ".");
-  } else {
-    alert("Too low! Guess a higher number, " + game.name + ".");
-  }
-}
-
-play();
+game.play();

37264d5da7c06b8972e2d5b3542dce02c14d09fd
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-10 16:13:58 -0700

    Used DOM manipulation to show hidden id='recipe' by guessing the right answer

--- scripts.js
+++ scripts.js
@@ -5,19 +5,11 @@ var Game = function(name){
   this.play = function() {
 
     var guess = parseInt(prompt("Hi " + this.name + "! How many ingredients are in my french toast recipe? If you get it right, I'll give you the recipe."));
-
-    var message,
-        recipe;
+    var recipe = document.getElementById("recipe");
 
     if (guess === this.answer) {
       alert("That's right! Let's have breakfast, " + this.name + ".");
-
-      message = "The ingredients are: 1 egg, 1 tsp vanilla, 1/2 tsp cinnamon, 1/4 cup milk, 4 slices of bread, butter, syrup. <br><br>";
-      message+= "Beat egg, vanilla and cinnamon in a bowl. Pour in milk. Dip bread in mixture and cook on medium heat until brown on both sides. <br><br>";
-      message+= "Top with butter and syrup.";
-
-      recipe = document.getElementById("recipe");
-      recipe.innerHTML = message;
+      recipe.style.display = "block";
 
     } else if (guess > this.answer) {
       alert("Too high! Guess a lower number, " + this.name + ".");

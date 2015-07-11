3a95981e7bf6ae25932c91758bb488d7c5432cdf
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-10 21:33:49 -0700

    Changed alert() functions to DOM functions that added new text to the page

--- scripts.js
+++ scripts.js
@@ -5,22 +5,22 @@ var Game = function(name){
   this.play = function() {
 
     var guess = parseInt(prompt("Hi " + this.name + "! How many ingredients are in my french toast recipe? If you get it right, I'll give you the recipe."));
-    var recipe = document.getElementById("recipe");
+    var paragraph = document.getElementsByTagName("p");
 
     if (guess === this.answer) {
-      alert("That's right! Let's have breakfast, " + this.name + ".");
-      recipe.style.display = "block";
+      paragraph[0].innerHTML = "That's right, " + this.name + "! Let's have breakfast!";
+      document.getElementById("recipe").style.display = "block";
 
     } else if (guess > this.answer) {
-      alert("Too high! Guess a lower number, " + this.name + ".");
+      paragraph[0].innerHTML = "Too high! Guess a lower number, " + this.name + ".";
       this.play();
 
     } else if (guess < this.answer) {
-      alert("Too low! Guess a higher number, " + this.name + ".");
+      paragraph[0].innerHTML = "Too low! Guess a higher number, " + this.name + ".";
       this.play();
 
     } else {
-      alert("C'mon " + this.name + ", that's not a number!");
+      paragraph[0].innerHTML = "C'mon " + this.name + ", that's not a number!";
       this.play();
     }
   };

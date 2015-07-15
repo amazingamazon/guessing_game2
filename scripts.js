cd8faeb67215197fe38fe9362a4f9d839fbeb589
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-14 22:26:18 -0700

    Created elements that told players what their guess was and appended them to the DOM.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ var Game = function(name){
 
     var guess = parseInt(prompt("Hi " + this.name + "! How many ingredients are in my french toast recipe? If you get it right, I'll give you the recipe."));
     var paragraph = document.getElementsByTagName("p");
+    var createParagraph = document.createElement("p");
 
     if (guess === this.answer) {
       paragraph[0].innerHTML = "That's right, " + this.name + "! Let's have breakfast!";
@@ -13,19 +14,25 @@ var Game = function(name){
 
     } else if (guess > this.answer) {
       paragraph[0].innerHTML = "Too high! Guess a lower number, " + this.name + ".";
+      paragraph[0].appendChild(createParagraph);
+      paragraph[1].textContent = "You guessed " + guess + ". Better luck next time.";
       this.play();
 
     } else if (guess < this.answer) {
       paragraph[0].innerHTML = "Too low! Guess a higher number, " + this.name + ".";
+      paragraph[0].appendChild(createParagraph);
+      paragraph[1].textContent = "You guessed " + guess + ". Better luck next time.";
       this.play();
 
     } else {
       paragraph[0].innerHTML = "C'mon " + this.name + ", that's not a number!";
+      paragraph[0].appendChild(createParagraph);
+      paragraph[1].textContent = "It's a good thing we're friends."
       this.play();
     }
   };
 };
 
-var game = new Game("Jeff");
+var game = new Game("Dale");
 
 game.play();
